9f5f9c0cd56863ce09f8756b570ccb41bc1ac331
2017A7PS0297G <f20170297@goa.bits-pilani.ac.in>
2019-09-08 18:08:35 +0530

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RC Assignment 1
+
+Use the following symbols for the operators:
+
+'.': AND
+'+': OR
+'!': NOT
+'>': IMPLICATION
+'<': BI-IMPLICATION
--- formula_validity_checker/valid_symbols_dictionary.py
+++ formula_validity_checker/valid_symbols_dictionary.py
@@ -2,8 +2,4 @@ symbols_dict = {'.': 'AND',\
 				'+': 'OR',\
 				'!': 'NOT',\
 				'>': 'IMPLICATION',\
-				'<': 'DOUBLE_IMPLICATION'}
-
-# string = "hello"
-
-# print(string[5])
+				'<': 'BI_IMPLICATION'}
